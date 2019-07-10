e7399d7d70bc5cdeec279cd2e2290cafd8d7490b
Zerneax <d.chris59@gmail.com>
2019-07-10 22:54:12 +0200

    add stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { NavigationComponent } from './pages/navigation/navigation.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
